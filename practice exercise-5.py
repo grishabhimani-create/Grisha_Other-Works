74d6e41f758e6d128fd73ccb2eddb38d93ae0bed
grishabhimani-create <grishabhimani@gmail.com>
2026-05-24 12:57:49 +0530

    Add files via upload

---
+++ practice exercise-5.py
@@ -0,0 +1,10 @@
+# prectice exercise
+
+# Convert Celsius to Fahrenheit
+
+celsius = float(input("Enter temperature in Celsius: "))
+
+fahrenheit = (celsius * 9/5) + 32
+
+print("Temperature in Fahrenheit =", fahrenheit)
+
