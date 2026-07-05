d48fde75798eb1ce3d18640e8674eb22a4c00742
grishabhimani-create <grishabhimani@gmail.com>
2026-07-05 11:22:01 +0530

    Add files via upload

---
+++ task in python3.py
@@ -0,0 +1,17 @@
+# 1. Integer (Whole number)
+age = 16
+
+# 2. Float (Decimal number)
+gpa = 3.85
+
+# 3. String (Text)
+language = "Python"
+
+# 4. Boolean (True or False)
+is_fun = True
+
+# Printing each variable and its datatype
+print("Variable:", age, "| Type:", type(age))
+print("Variable:", gpa, "| Type:", type(gpa))
+print("Variable:", language, "| Type:", type(language))
+print("Variable:", is_fun, "| Type:", type(is_fun))
